7dcaa31116bcfc5eacdd7d09914ce747ec99e2c2
yomesh <yomesh6118111@gmail.com>
2023-03-13 00:38:33 +0530

    iniy

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -24,8 +24,12 @@ class Graph extends Component<IProps, {}> {
 
     const schema = {
       stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert:'float',
       timestamp: 'date',
     };
 
@@ -36,13 +40,16 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      // elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio:'avg',
+        upper_bound:'avg',
+        lower_bound:'avg',
         timestamp: 'distinct count',
       }));
     }
@@ -50,9 +57,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([DataManipulator.generateRow(this.props.data),] as unknown as TableData );
     }
   }
 }
